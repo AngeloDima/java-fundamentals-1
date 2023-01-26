bfe6f59cf30601d23d289acf358a9ccf2f35b3e4
AngeloDima <Angelodimauro91@gmail.com>
2023-01-26 14:59:12 +0100

    FizzBuzz finito (For)

--- src/org/lessons/java/security/FizzBuzz.java
+++ src/org/lessons/java/security/FizzBuzz.java
@@ -9,9 +9,23 @@ public class FizzBuzz {
 		
 		//Scrivo da 1 a 100
 		
-		Random generatore = new Random();
-		int numeri = generatore.nextInt(100)+1;
-		System.out.println(numeri);
-	}
+		for (int numeri = 0; numeri < 100; numeri++) {
+			
+		//Multipli di 3 e 5 stampo FizzBuzz
+		if (numeri % 3 == 0 && numeri % 5 == 0) {
+			System.out.println("il tuo numero è " + numeri + " FizzBuzz");
+			
+			//Multipli di 3 e 5 stampo Buzz
+		} else if (numeri % 5 == 0) {
+			System.out.println("il tuo numero è " + numeri + " Buzz");
+		} else if (numeri % 3 == 0) {
+			
+			//Multipli di 3 e 5 stampo Fizz
+			System.out.println("il tuo numero è " + numeri + " Fizz");
+		} else {
+			System.out.println("il tuo numero è " + numeri);
+		}
+	 }
+  }
 
 }
