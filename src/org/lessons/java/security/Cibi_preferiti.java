4983375ed7593babf6f3f078ccdd0ee96adbde16
AngeloDima <Angelodimauro91@gmail.com>
2023-01-26 14:41:24 +0100

    Cibi preferiti Fine

---
+++ src/org/lessons/java/security/Cibi_preferiti.java
@@ -0,0 +1,16 @@
+package org.lessons.java.security;
+
+public class Cibi_preferiti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] CibiPreferiti = {"Pizza", "Panino con carne di cavallo", "gelato", "Patate" ,"Pollo"};
+		
+		System.out.println("Lunghezza Array: " + CibiPreferiti.length + " elementi");
+		System.out.println("Cibo super preferito " + CibiPreferiti[0]);
+		System.out.println("Cibo meno preferito " + CibiPreferiti[4]);
+
+	}
+
+}
