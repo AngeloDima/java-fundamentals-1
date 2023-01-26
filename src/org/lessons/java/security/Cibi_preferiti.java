44de3b7bd63ddb0c619df0977823d2ad34550d68
AngeloDima <Angelodimauro91@gmail.com>
2023-01-26 15:30:14 +0100

    Fix

--- src/org/lessons/java/security/Cibi_preferiti.java
+++ src/org/lessons/java/security/Cibi_preferiti.java
@@ -7,10 +7,21 @@ public class Cibi_preferiti {
 
 		String[] CibiPreferiti = {"Pizza", "Panino con carne di cavallo", "gelato", "Patate" ,"Pollo"};
 		
-		System.out.println("Lunghezza Array: " + CibiPreferiti.length + " elementi");
-		System.out.println("Cibo super preferito " + CibiPreferiti[0]);
-		System.out.println("Cibo meno preferito " + CibiPreferiti[4]);
+		//System.out.println("Lunghezza Array: " + CibiPreferiti.length + " elementi");
+		//System.out.println("Cibo super preferito " + CibiPreferiti[0]);
+		//System.out.println("Cibo meno preferito " + CibiPreferiti[4]);
 
-	}
-
+		for (int i = 0; i < CibiPreferiti.length; i++) {
+			 if (i == 0) {
+				 System.out.println("Cibo super preferito: " + CibiPreferiti[i]);
+			     
+		      }
+	     }
+		for (int i = 0; i < CibiPreferiti.length; i++) {
+		    if (i == CibiPreferiti.length - 1) {
+		        System.out.println("Cibo meno preferito: " + CibiPreferiti[i]);
+		        
+		    }
+		}
+  }
 }
